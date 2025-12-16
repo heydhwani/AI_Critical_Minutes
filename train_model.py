3a9bbe4c7796f35076ececf848fa9a1459ada897
Dhwani <dhwanijain731@gmail.com>
2025-12-16 22:15:48 +0530

    saved trained model

--- train_model.py
+++ train_model.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
+import joblib
+import os
 
 # Load dataset
 df = pd.read_csv("data/golden_ai_emergency_dataset.csv")
@@ -48,3 +50,12 @@ print(confusion_matrix(y_test, y_pred))
 
 print("\nCLASSIFICATION REPORT:")
 print(classification_report(y_test, y_pred))
+
+
+# Create models directory if not exists
+os.makedirs("models", exist_ok=True)
+
+# Save trained model
+joblib.dump(model, "models/emergency_risk_model.joblib")
+
+print("Model saved at models/emergency_risk_model.joblib")
