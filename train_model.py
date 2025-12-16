19053d6e97966fd7279452146d7272c7ff2e20b1
Dhwani <dhwanijain731@gmail.com>
2025-12-16 21:51:28 +0530

    splitted data into training and testing

---
+++ train_model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Load dataset
+df = pd.read_csv("data/golden_ai_emergency_dataset.csv")
+
+# Features & target
+X = df[
+    [
+        "age",
+        "heart_rate",
+        "systolic_bp",
+        "diastolic_bp",
+        "spo2",
+        "respiration_rate",
+        "body_temp",
+        "blood_sugar"
+    ]
+]
+
+y = df["emergency_risk_level"]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+print("Train shape:", X_train.shape)
+print("Test shape:", X_test.shape)
