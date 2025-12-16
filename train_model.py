7cd4ca0b3293395f66c11d03b05e3fddd2d8f16b
Dhwani <dhwanijain731@gmail.com>
2025-12-16 21:59:06 +0530

    applied random forest

--- train_model.py
+++ train_model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
 # Load dataset
 df = pd.read_csv("data/golden_ai_emergency_dataset.csv")
@@ -28,5 +30,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-print("Train shape:", X_train.shape)
-print("Test shape:", X_test.shape)
+# Random Forest Model
+
+model = RandomForestClassifier(
+    n_estimators=200,
+    max_depth=10,
+    class_weight="balanced",
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
